9b0009152917454b573fb330ae9c2111c25d23ab
Haresh <hareshlive2001@gmail.com>
2024-06-22 18:57:29 +0530

    Maze runner

---
+++ Zoho_AdvancedProgramming_Round3/src/mazeRunner/Main.java
@@ -0,0 +1,12 @@
+package mazeRunner;
+
+public class Main {
+    public static void main(String[] args) {
+        Maze maze = new Maze(6, 6);
+        maze.putTreasure(2, 4);
+//        maze.printMaze();
+        int shortestPath = maze.shortestPath(5, 1);
+        maze.printMaze();
+        System.out.println("The shortest path is : " + shortestPath);
+    }
+}
